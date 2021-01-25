09ea2d93b73d67167e8be900fd7d266d9419fe7a
ZameerParvez <zp00011001@gmail.com>
2021-01-25 20:59:20 +0000

    Add solution to days hotter problem

---
+++ Algorithm Practice/Algorithms/0006-DaysUntilHotter.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include<vector>
+
+#include "Algorithm.h"
+
+namespace Algorithms {
+
+// This algorithm returns an array giving the number of days until the next hottest day, where the day is indicated by the index of the input list of temperatures
+class DaysUntilHotter : public Algorithm {
+ private:
+  std::vector<int> tempsOnDays;  
+ public:
+  int parseInput(istream& input) override;
+  int solve(ostream& output) override;
+};
+
+}  // namespace Algorithms
